ecd031f15996ca90cdd169d05116e6a829160344
KC <kcmacha0521@gmail.com>
2026-04-24 15:39:16 +0800

    change compiler

--- compile.sh
+++ compile.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 clear
-g++ Main.cpp Lexer.cpp -o Main.obj
+
+# G++: The classic GNU compiler (mature optimizations for the final binary)
+# G++: Use for final testing (usually generates the fastest execution)
+# g++ Main.cpp Lexer.cpp -o Main.obj
+
+# Clang++: The LLVM-based compiler (modular design, great for tooling)
+# Clang++: Use for development (faster builds and clearer error messages)
+clang++ Main.cpp Lexer.cpp -o Main.obj
+
 ./Main.obj
